80535ceb4956a872a3fb4f77051ef156f10148ff
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-20 19:23:22 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,6 +7,6 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   def cobble
     puts "Your shoe is as good as new!"
-    shoe.condition = "new"
+    Shoe.condition = "new"
   end
 end
